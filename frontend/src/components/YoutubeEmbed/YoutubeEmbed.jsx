1f04a404eb843e9d7dc4a960a1e27b93f425cdc1
Abdullah <abdullahan1928@gmail.com>
2022-08-13 14:19:21 +0500

    Added autoplay to youtube embedded videos

--- frontend/src/components/YoutubeEmbed/YoutubeEmbed.jsx
+++ frontend/src/components/YoutubeEmbed/YoutubeEmbed.jsx
@@ -4,7 +4,7 @@ const YoutubeEmbed = ({ embedId }) => (
     <iframe
         width="1130"
         height="500"
-        src={`https://www.youtube.com/embed/${embedId}`}
+        src={`https://www.youtube.com/embed/${embedId}?autoplay=1`}
         frameBorder="0"
         sandbox="allow-scripts allow-presentation allow-same-origin"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
